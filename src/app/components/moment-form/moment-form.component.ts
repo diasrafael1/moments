734fb9c034d15b5666810c8f885cfe09a838588e
Rafael Dias <rafaelldiias100@gmail.com>
2022-10-30 23:52:56 -0300

    Salvar dado do input de imagens

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -29,9 +29,15 @@ export class MomentFormComponent implements OnInit {
     return this.momentForm.get("description")!;
   }
 
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+
+    this.momentForm.patchValue({ image: file });
+  }
+
   submit() {
     if (this.momentForm.invalid) return;
 
-    console.log("Envio de formulário");
+    console.log(this.momentForm.value);
   }
 }
