7c1b4cfc797586dedba0a0eef7a1a1c9e44c2ee8
Rafael Dias <rafaelldiias100@gmail.com>
2022-11-05 19:56:23 -0300

    feito o form group

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from "@angular/core";
 import { Router, ActivatedRoute } from "@angular/router";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormGroupDirective,
+} from "@angular/forms";
 import { MomentService } from "src/app/services/moment.service";
+import { CommentService } from "src/app/services/comment.service";
 import { IMoment } from "src/app/interfaces/Moment";
+import { IComment } from "src/app/interfaces/Comment";
 import { environment } from "src/environments/environment";
 import { faTimes, faEdit } from "@fortawesome/free-solid-svg-icons";
 import { MessagesService } from "src/app/services/messages.service";
@@ -16,9 +24,11 @@ export class MomentComponent implements OnInit {
   baseApiUrl = environment.baseApiUrl;
   faTimes = faTimes;
   faEdit = faEdit;
+  commentForm!: FormGroup;
 
   constructor(
     private momentService: MomentService,
+    private commentService: CommentService,
     private route: ActivatedRoute,
     private router: Router,
     private messagesService: MessagesService
@@ -30,6 +40,18 @@ export class MomentComponent implements OnInit {
     this.momentService
       .getMoment(id)
       .subscribe((item) => (this.moment = item.data));
+
+    this.commentForm = new FormGroup({
+      text: new FormControl("", [Validators.required]),
+      username: new FormControl("", [Validators.required]),
+    });
+  }
+
+  get text() {
+    return this.commentForm.get("text")!;
+  }
+  get username() {
+    return this.commentForm.get("username")!;
   }
 
   async removeHandler(id: number) {
@@ -38,4 +60,6 @@ export class MomentComponent implements OnInit {
     this.messagesService.add("Momento excluído com sucesso!");
     this.router.navigate(["/"]);
   }
+
+  onSubmit(formDirective: FormGroupDirective) {}
 }
