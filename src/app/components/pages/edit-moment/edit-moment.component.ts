95bc06fb2906a119654ae36b809b9950080806af
Rafael Dias <rafaelldiias100@gmail.com>
2022-11-05 18:18:24 -0300

    feita a funcionalidade de editar momento

--- src/app/components/pages/edit-moment/edit-moment.component.ts
+++ src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { IMoment } from "src/app/interfaces/Moment";
 import { MomentService } from "src/app/services/moment.service";
+import { MessagesService } from "src/app/services/messages.service";
 
 @Component({
   selector: "app-edit-moment",
@@ -15,7 +16,8 @@ export class EditMomentComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private momentService: MomentService
+    private momentService: MomentService,
+    private messagesService: MessagesService
   ) {}
 
   ngOnInit(): void {
@@ -24,5 +26,22 @@ export class EditMomentComponent implements OnInit {
     this.momentService
       .getMoment(id)
       .subscribe((item) => (this.moment = item.data));
+  }
+
+  async editHandler(momentData: IMoment) {
+    const id = this.moment.id;
+    const formData = new FormData();
+
+    formData.append("title", momentData.title);
+    formData.append("description", momentData.description);
+
+    if (momentData.image) {
+      formData.append("image", momentData.image);
+    }
+
+    await this.momentService.updateMoment(id!, formData).subscribe();
+
+    this.messagesService.add(`Momento ${id} alterado com sucesso!`);
+    this.router.navigate(["/"]);
   }
 }
