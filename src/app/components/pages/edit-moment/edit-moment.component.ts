e22a076901664109beb27ad64bfb53994423641d
Rafael Dias <rafaelldiias100@gmail.com>
2022-11-05 17:30:54 -0300

    feita a funcionalidade de pegar os dados do moment na api

--- src/app/components/pages/edit-moment/edit-moment.component.ts
+++ src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { IMoment } from "src/app/interfaces/Moment";
+import { MomentService } from "src/app/services/moment.service";
 
 @Component({
-  selector: 'app-edit-moment',
-  templateUrl: './edit-moment.component.html',
-  styleUrls: ['./edit-moment.component.css']
+  selector: "app-edit-moment",
+  templateUrl: "./edit-moment.component.html",
+  styleUrls: ["./edit-moment.component.css"],
 })
 export class EditMomentComponent implements OnInit {
+  moment!: IMoment;
+  btnText = "Editar";
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private momentService: MomentService
+  ) {}
 
   ngOnInit(): void {
-  }
+    const id = Number(this.route.snapshot.paramMap.get("id"));
 
+    this.momentService
+      .getMoment(id)
+      .subscribe((item) => (this.moment = item.data));
+  }
 }
