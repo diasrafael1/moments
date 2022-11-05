8c7e5386b05d229942a6776ac048726a0c2391f7
Rafael Dias <rafaelldiias100@gmail.com>
2022-11-05 19:56:11 -0300

    feito o service de criar comentario

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+import { environment } from "src/environments/environment";
+import { IComment } from "../interfaces/Comment";
+import { IResponse } from "../interfaces/Response";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CommentService {
+  private baseApiUrl = environment.baseApiUrl;
+  private apiUrl = `${this.baseApiUrl}api/moments/comments`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  createComment(data: IComment): Observable<IResponse<IComment>> {
+    return this.http.post<IResponse<IComment>>(this.apiUrl, data);
+  }
 }
