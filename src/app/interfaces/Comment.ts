ca89575a0f97b7929e07d8e665688c2a517c9506
Rafael Dias <rafaelldiias100@gmail.com>
2022-11-05 19:42:01 -0300

    correção nos nomes das interfaces

--- src/app/interfaces/Comment.ts
+++ src/app/interfaces/Comment.ts
@@ -1,4 +1,4 @@
-export interface Comment {
+export interface IComment {
   id?: string;
   text: string;
   username: string;
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { IMoment } from "../interfaces/Moment";
-import { Response } from "../interfaces/Response";
+import { IResponse } from "../interfaces/Response";
 
 @Injectable({
   providedIn: "root",
@@ -18,12 +18,12 @@ export class MomentService {
     return this.http.post<FormData>(this.apiUrl, formData);
   }
 
-  getMoments(): Observable<Response<IMoment[]>> {
-    return this.http.get<Response<IMoment[]>>(this.apiUrl);
+  getMoments(): Observable<IResponse<IMoment[]>> {
+    return this.http.get<IResponse<IMoment[]>>(this.apiUrl);
   }
 
-  getMoment(id: number): Observable<Response<IMoment>> {
-    return this.http.get<Response<IMoment>>(`${this.apiUrl}/${id}`);
+  getMoment(id: number): Observable<IResponse<IMoment>> {
+    return this.http.get<IResponse<IMoment>>(`${this.apiUrl}/${id}`);
   }
 
   removeMoment(id: number) {
